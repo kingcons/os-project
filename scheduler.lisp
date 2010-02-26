2d743157abcba06606a1cef16abe6d005b98d97b
Justin Caratzas <justin.caratzas@gmail.com>
2010-02-26 00:41:52 -0500

    add job unloading from ram and helper functions

--- scheduler.lisp
+++ scheduler.lisp
@@ -11,7 +11,32 @@
   (vector-push job-id *ready-queue*))
 
 ;; unloads job from RAM and stores in back to disk
-;;(defun unload-job (job-id))
+(defun unload-job (job-id)
+  (let* ((current-job (gethash (write-to-string job-id) *pcb*))
+	 (num (+ (data-buffer current-job)
+		 (data-count current-job)))
+	 (start-ram-pos (+ (start-ram current-job) (ins-count current-job)))
+	 (start-disk-pos (+ (start-disk current-job) (ins-count current-job))))
+    (do ((index start-ram-pos (+ index 1))
+	 (disk-place start-disk-pos (+ disk-place 1)))
+	((equal index (+ start-ram-pos num)) (format t "~D words unloaded from RAM" num) t)
+      (memory-write *disk* disk-place (memory-read *memory* index))
+      (format t "~D copied from memory to disk~%" (memory-read *memory* index))
+      (memory-write *memory* index 0))))
+
+(defun job-total-space (job)
+  (+ (ins-count job)
+      (data-buffer job)
+      (data-count job)
+      (scratchpad job)))
+
+(defun clear-ram (job)
+  (let ((num (job-total-space job))
+	(start-pos (start-ram job)))
+    (do ((index start-pos (+ index 1)))
+	((eql index (+ start-pos num)) (format t "~D words unloaded from RAM" num) t)
+      (memory-write *memory* index 0))
+    (setf (start-ram job) -1)))
 
 ;; loads job from disk into RAM
 ;; since we're assuming 1 job at a time for phase 1,
