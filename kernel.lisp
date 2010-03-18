bfdbbba41fe473cbea5854f41cd95867f28d423a
Brit Butler <redline6561@gmail.com>
2010-03-18 13:38:06 -0400

    Kernel cleanup and debugging function.

--- kernel.lisp
+++ kernel.lisp
@@ -7,7 +7,21 @@
   (long-scheduler)
   (short-scheduler *cpu1*))
 
-(defun os-driver () ; or main/kernel
-  (init)
+(defun kernel ()
   (loop (fetch *cpu1*)
 	(decode *cpu1*)))
+
+(defun os-driver ()
+  (init)
+  (kernel))
+
+(defun debugging (setting)
+  (ecase setting
+    (:on
+       (trace memory-read memory-write
+	      register-read register-write
+	      fetch decode))
+    (:off
+       (untrace memory-read memory-write
+		register-read register-write
+		fetch decode))))
