85f2b2caec247994b756f82e9f04f67ae25b7ae3
Brit Butler <redline6561@gmail.com>
2010-03-18 11:38:16 -0400

    Add support for scheduler writing jobs back to disk. *Untested*

--- scheduler.lisp
+++ scheduler.lisp
@@ -23,23 +23,33 @@
     (loop for job-id = (car (pop *job-order*)) while job-id
 	  for job = (gethash job-id *pcb*)
 	  until (> (job-total-space job) (memory-free *memory*))
-	  do (load-job job)
+	  do (move-job job :type :load)
 	     (cl-heap:enqueue *ready-queue* job-id priority)
 	     (incf priority))))
 
-(defun load-job (job)
-  (setf (start-ram job) (memory-index *memory*))
-  (loop for i from (start-disk job) to (1- (+ (start-disk job)
-					      (job-total-space job)))
-	do (memory-push-end *memory* (memory-read *disk* i)))
-  (setf (status job) :in-memory)
-  (format t "~d words loaded into RAM.~%" (job-total-space job)))
+(defun move-job (job &key type)
+  (let ((start-ram (start-ram job))
+	(start-disk (start-disk job))
+	(total-space (job-total-space job)))
+    (ecase type
+      (:load
+	 (setf (start-ram job) (memory-index *memory*))
+	 (loop for i from start-disk to (1- (+ start-disk total-space))
+	       do (memory-push-end *memory* (memory-read *disk* i)))
+	 (setf (status job) :in-memory)
+	 (format t "~d words loaded into RAM.~%" total-space))
+      (:save
+	 (loop for i from start-disk to (1- (+ start-disk total-space))
+	       for j from start-ram to (1- (+ start-ram total-space))
+	       do (memory-write *disk* i (memory-read *memory* j)))
+	 (setf (status job) :in-disk)
+	 (format t "~d words saved to disk.~%" total-space)))))
 
 (defun short-scheduler (cpu)
   (let* ((job-id (cl-heap:dequeue *ready-queue*))
 	 (job (gethash job-id *pcb*)))
-    (when nil ; *context-switch-p*
-      (context-switch))
+;    (when nil ; *context-switch-p*?
+;      (context-switch))
     (dispatcher job job-id cpu)))
 
 (defun dispatcher (job job-id cpu)
